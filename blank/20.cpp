ec308ddcbc9002347923b8ceb1e56455a521836e
Griger5 <griger04@gmail.com>
2026-05-26 19:32:10 +0200

    add very fun exercise

---
+++ blank/20.cpp
@@ -0,0 +1,26 @@
+#include <utility>
+
+/* MIEJSCE NA KOD */ unsigned factorial /* MIEJSCE NA KOD */ {
+    if (n == 1) return 1;
+    return n * factorial(n-1);
+}
+
+template <typename T, unsigned N>
+struct array
+/* 
+    MIEJSCE NA KOD
+*/
+
+int main() {
+    array<int,6> a;
+    const array<int,factorial(3)> b;
+    array<int, factorial(4)> c;
+    auto d = std::move(c);
+    a=b;
+    a=std::move(b);
+    static_assert(a.size()==6);
+
+    static_assert(array<unsigned,factorial(4)>::size()==24);
+    static_assert(array<unsigned,factorial(3)>::size() == b.size());
+    // static_assert(array<int, factorial(4)>::size()==b.size()); //blad kompilacji
+}
